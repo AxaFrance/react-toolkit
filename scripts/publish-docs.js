7fa39488530a5d2aabe2bcd4a7dfd309b8e7f3c6
Guillaume Chervet <43414465+guillaumechervetaxa@users.noreply.github.com>
2019-02-15 10:51:16 +0000

    chore(demo) fix deployment (#75)

--- scripts/publish-docs.js
+++ scripts/publish-docs.js
@@ -1,43 +1,65 @@
-
 const fs = require('fs-extra');
 const args = process.argv;
 const VERSION = require('../storybook/storybook/package.json').version;
 const GITHUB_TOKEN = args[2];
 
 try {
-
-    const dir = './tmp';
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-    }
-
-    const execSync = require('child_process').execSync;
-    let child = execSync(`git clone https://toto:${GITHUB_TOKEN}@github.com/AxaGuilDEv/AxaGuilDEv.github.io ./tmp/AxaGuilDEv.github.io/`);
-    console.log('error', child.error);
-    console.log('stdout ', child.stdout);
-    console.log('stderr ', child.stderr);
-
-    const PREVIOUS_VERSION = require('./tmp/AxaGuilDEv.github.io/react-toolkit/latest/version.json').version;
-    
-    fs.moveSync('./tmp/AxaGuilDEv.github.io/react-toolkit/latest', `./tmp/AxaGuilDEv.github.io/react-toolkit/v${PREVIOUS_VERSION}`);
-
-    const environment =  require(`./tmp/AxaGuilDEv.github.io/react-toolkit/v${PREVIOUS_VERSION}/demo/environment.json`);
-    environment.baseUrl = `/react-toolkit/v${PREVIOUS_VERSION}/demo/`;
-
-    fs.writeFileSync(`./tmp/AxaGuilDEv.github.io/react-toolkit/v${PREVIOUS_VERSION}/demo/environment.json`, JSON.stringify(environment), 'utf8');
-
-    fs.copySync(`./storybook/styles/distDemo`, `./tmp/AxaGuilDEv.github.io/react-toolkit/latest/design`);
-    fs.copySync(`./storybook/storybook/storybook-static`, `./tmp/AxaGuilDEv.github.io/react-toolkit/latest/storybook`);
-    fs.copySync(`./examples/demo/build`, `./tmp/AxaGuilDEv.github.io/react-toolkit/latest/demo`);
-    const versionToSave = { version: VERSION };
-    fs.writeJSONSync('./tmp/AxaGuilDEv.github.io/react-toolkit/latest/version.json', versionToSave);
-
-    child = execSync(`cd ./tmp/AxaGuilDEv.github.io/ && git add . && git commit -m "doc(toolkit) publish website ${VERSION}" && git push`);
-    console.log('error', child.error);
-    console.log('stdout ', child.stdout);
-    console.log('stderr ', child.stderr);
-
-    console.log('success!')
-  } catch (err) {
-    console.error(err)
+  const dir = './tmp';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
   }
+
+  const execSync = require('child_process').execSync;
+  let child = execSync(
+    `git clone https://toto:${GITHUB_TOKEN}@github.com/AxaGuilDEv/AxaGuilDEv.github.io ./tmp/AxaGuilDEv.github.io/`
+  );
+  console.log('error', child.error);
+  console.log('stdout ', child.stdout);
+  console.log('stderr ', child.stderr);
+
+  const PREVIOUS_VERSION = require('../tmp/AxaGuilDEv.github.io/react-toolkit/latest/version.json')
+    .version;
+
+  fs.moveSync(
+    './tmp/AxaGuilDEv.github.io/react-toolkit/latest',
+    `./tmp/AxaGuilDEv.github.io/react-toolkit/v${PREVIOUS_VERSION}`
+  );
+
+  const environment = require(`../tmp/AxaGuilDEv.github.io/react-toolkit/v${PREVIOUS_VERSION}/demo/environment.json`);
+  environment.baseUrl = `/react-toolkit/v${PREVIOUS_VERSION}/demo/`;
+
+  fs.writeFileSync(
+    `./tmp/AxaGuilDEv.github.io/react-toolkit/v${PREVIOUS_VERSION}/demo/environment.json`,
+    JSON.stringify(environment),
+    'utf8'
+  );
+
+  fs.copySync(
+    `./storybook/styles/distDemo`,
+    `./tmp/AxaGuilDEv.github.io/react-toolkit/latest/design`
+  );
+  fs.copySync(
+    `./storybook/storybook/storybook-static`,
+    `./tmp/AxaGuilDEv.github.io/react-toolkit/latest/storybook`
+  );
+  fs.copySync(
+    `./examples/demo/build`,
+    `./tmp/AxaGuilDEv.github.io/react-toolkit/latest/demo`
+  );
+  const versionToSave = { version: VERSION };
+  fs.writeJSONSync(
+    './tmp/AxaGuilDEv.github.io/react-toolkit/latest/version.json',
+    versionToSave
+  );
+
+  child = execSync(
+    `cd ./tmp/AxaGuilDEv.github.io/ && git add . && git commit -m "doc(toolkit) publish website ${VERSION}" && git push`
+  );
+  console.log('error', child.error);
+  console.log('stdout ', child.stdout);
+  console.log('stderr ', child.stderr);
+
+  console.log('success!');
+} catch (err) {
+  console.error(err);
+}
