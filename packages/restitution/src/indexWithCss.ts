5d973f7d1af318a26afa03eb4bfd39ef8ce5f854
guillaume chervet <guillaume.chervet@gmail.com>
2019-07-22 11:36:08 +0200

    maj

--- packages/restitution/src/indexWithCss.ts
+++ packages/restitution/src/indexWithCss.ts
@@ -1,10 +1,4 @@
 import './af-restitution.css';
 
-export { 
-    Restitution, 
-    SectionRestitutionColumn, 
-    SectionRestitutionRow, 
-    SectionRestitutionTitle, 
-    SectionRestitution, 
-    HeaderRestitution, 
-    ArticleRestitution } from './index';
+export { Restitution, SectionRestitutionColumn, SectionRestitutionRow, SectionRestitutionTitle,
+SectionRestitution, HeaderRestitution, ArticleRestitution } from './index';
