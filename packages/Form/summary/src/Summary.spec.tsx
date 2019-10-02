0af87e675c765e80c1439ac843906274a6a1eb5a
Xavier Balloy <xavier.balloy@pm.me>
2019-10-02 09:14:22 +0200

    feat(react-toolkit-form-summary): migrate to TypeScript

---
+++ packages/Form/summary/src/Summary.spec.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import { Summary } from './index';
+
+describe('<Summary>', () => {
+  it('Renders correctly with default props', () => {
+    const tree = renderer
+      .create(
+        <Summary/>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders correctly when not visible', () => {
+    const tree = renderer
+      .create(
+        <Summary isVisible={false}/>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Renders correctly when visible', () => {
+    const messages = ['Sample message 1', '', 'Sample message 2', null];
+    const tree = renderer
+      .create(
+        <Summary messages={messages} isVisible={true}/>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
