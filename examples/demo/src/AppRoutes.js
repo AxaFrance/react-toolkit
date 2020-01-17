a06d730f234a67a2b642c5dc9b53fabad26e32ab
guillaume chervet <guillaume.chervet@gmail.com>
2020-01-17 11:33:40 +0100

    refactor(demo) domain and sub-route

--- examples/demo/src/AppRoutes.js
+++ examples/demo/src/AppRoutes.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Home from '../Home';
-import New from '../quote/New';
-import Confirm from '../quote/Confirm';
-import PageNotFound from '../NotFound';
+import Home from './Home';
+import Quote from './Quote';
+import PageNotFound from './NotFound';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/new" component={New} />
-      <Route exact path="/confirm" component={Confirm} />
+      <Route path="/quote" component={Quote} />
       <Route component={PageNotFound} />
     </Switch>
   );
