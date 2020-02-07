ccbbf06b9c5da8455c7d9187990334f1cacff069
Arnaud Foraison <arnaud.foraison@gmail.com>
2020-02-07 19:25:57 +0100

    feat(example): add icons route

--- examples/demo/src/AppRoutes.js
+++ examples/demo/src/AppRoutes.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import Quote from './Quote';
 import PageNotFound from './NotFound';
+import Showroom from './Showroom/Showroom'
 
 const Routes = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/quote" component={Quote} />
+      <Route path="/icons" component={Showroom} />
       <Route component={PageNotFound} />
     </Switch>
   );
