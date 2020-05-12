ca68009cc6b2d53df883251620c5d29bfc8757e1
Xavier Balloy <xavier.balloy@pm.me>
2020-05-12 10:02:17 +0200

    feat(react-toolkit-table): allow to customize labels of Paging

--- storybook/storybook/src/packages/table/src/Paging/Paging.stories.js
+++ storybook/storybook/src/packages/table/src/Paging/Paging.stories.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { text, number } from '@storybook/addon-knobs';
+import { text, number, select } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
-import { Paging } from '@axa-fr/react-toolkit-table';
+import { Paging, Modes } from '@axa-fr/react-toolkit-table';
 import readme from '@axa-fr/react-toolkit-table/src/Paging/README.md';
 
 const PagingStory = () => (
   <Paging
-    numberItems={number('numberItems', 20)}
-    numberPages={number('numberPages', 23)}
-    currentPage={number('currentPage', 5)}
-    onChange={action('onChange')}
     classModifier={text('classModifier', '')}
     className={text('className', 'af-paging')}
+    currentPage={number('currentPage', 5)}
+    displayLabel={text('displayLabel', 'Show')}
+    elementsLabel={text('elementsLabel', 'elements')}
+    mode={select('mode', Modes, Modes.default)}
+    nextLabel={text('nextLabel', 'Next »')}
+    numberPages={number('numberPages', 23)}
+    ofLabel={text('ofLabel', 'of')}
+    onChange={action('onChange')}
+    previousLabel={text('previousLabel', '« Previous')}
+    numberItems={number('numberItems', 20)}
   />
 );
 
