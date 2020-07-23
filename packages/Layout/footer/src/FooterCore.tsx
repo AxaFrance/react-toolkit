ff3166e2b3b8b6dfa5c79b04ffbe913098130740
barry-thierno <45846776+barry-thierno@users.noreply.github.com>
2020-07-23 09:15:50 +0200

    feat: add props to footer component to hide icon (#120)

--- packages/Layout/footer/src/FooterCore.tsx
+++ packages/Layout/footer/src/FooterCore.tsx
@@ -5,50 +5,44 @@ import {
   WithClassModifierOptions,
   compose,
 } from '@axa-fr/react-toolkit-core';
+
 const DEFAULT_CLASSNAME = 'af-footer';
 
-interface FooterCoreComponentProps {
-  children?: React.ReactNode;
-  href?: string;
-  title?: string;
-  icon?: string;
-  alt?: string;
-  className?: string;
-}
-const defaultProps: Partial<FooterCoreComponentProps> = {
-  children: `© ${new Date().getFullYear()} AXA Tous droits réservés`,
+export type FooterCoreProps = Partial<typeof defaultProps> &
+  WithClassModifierOptions;
+
+const defaultProps = {
+  children: `© ${new Date().getFullYear()} AXA Tous droits réservés` as React.ReactNode,
   href: 'https://www.axa.fr/',
   title: 'Site Axa',
   icon: 'assets/logo-axa.svg',
   alt: 'Logo Axa',
+  isIconHidden: false,
 };
 
-const FooterCoreRaw: React.SFC<FooterCoreComponentProps> = ({
+const FooterCoreRaw = ({
   className,
   href,
   title,
   icon,
+  isIconHidden,
   alt,
   children,
-}) => (
+}: FooterCoreProps) => (
   <footer className={className}>
     <div className="container-fluid container">
-      <a className="af-logo" href={href} title={title} target="blank">
-        <img className="af-logo__brand" src={icon} alt={alt} />
-      </a>
+      {!isIconHidden && (
+        <a className="af-logo" href={href} title={title} target="blank">
+          <img className="af-logo__brand" src={icon} alt={alt} />
+        </a>
+      )}
       <div className="af-footer-content">{children}</div>
     </div>
   </footer>
 );
 FooterCoreRaw.defaultProps = defaultProps;
 
-export type FooterCoreProps = FooterCoreComponentProps &
-  WithClassModifierOptions;
-
-const enhance = compose<FooterCoreComponentProps, FooterCoreProps>(
-  withClassDefault(DEFAULT_CLASSNAME),
-  withClassModifier
-);
+const enhance = compose(withClassDefault(DEFAULT_CLASSNAME), withClassModifier);
 
 const Enhanced = enhance(FooterCoreRaw);
 Enhanced.displayName = 'FooterCore';
