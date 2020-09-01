fa66694e88af2fe29a1174b474f8acc3009a6682
Guillaume Chervet <guillaume.chervet@gmail.com>
2020-09-01 11:08:49 +0200

    fix(publish) update snapshot

--- scripts/npm.js
+++ scripts/npm.js
@@ -8,9 +8,11 @@ try {
     if(args.length >= 3){
         const NPM_TOKEN = args[2];
         template = `registry=https://registry.npmjs.org/
-//registry.npmjs.org/:_authToken=${NPM_TOKEN}`;
+//registry.npmjs.org/:_authToken=${NPM_TOKEN}
+save-exact=true`;
     } else {
-        template = 'registry=https://registry.npmjs.org/';
+        template = `registry=https://registry.npmjs.org/
+save-exact=true`;
     }
    const filePath = path.join(__dirname, '../.npmrc');
    fs.writeFileSync(filePath  , template, 'utf8');
