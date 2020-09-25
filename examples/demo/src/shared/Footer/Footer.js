6621823985c337458303e31573e7e14027a70ff8
Samuel Gomez <43414022+samuel-gomez-axa@users.noreply.github.com>
2020-09-25 09:02:48 +0200

    feat(demo): delete demo (#685)
    * feat(demo): delete demo
    
    * fix(storybook): install dependancy classnames
    
    * fix(design): cuid dep
    
    * docs(demo): delete demo informations
    
    * test(snap): regenerate help and summary
    
    Co-authored-by: Samuel Gomez <contact@samuelgomez.fr>

--- examples/demo/src/shared/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logo from '@axa-fr/react-toolkit-core/dist/assets/logo-axa.svg';
-import { Footer } from '@axa-fr/react-toolkit-all';
-import './Footer.scss';
-const FooterApp = () => {
-  return <Footer icon={logo} />;
-};
-
-export default FooterApp;
--- examples/demo/src/withProps.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const withProps = (callback) => (Component) => (props) => {
-  const newProps =  callback(props);
-  return (<Component {...props} {...newProps}  />);
-};
-
-export default withProps;
