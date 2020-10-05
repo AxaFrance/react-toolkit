d27e4258fd9ac5714acdb3064786f51a70ff0f9e
Samuel Gomez <contact@samuelgomez.fr>
2020-10-06 01:05:51 +0200

    feat(index): add general styles and headerlinks instance

--- storybook/design-system/src/index.js
+++ storybook/design-system/src/index.js
@@ -1,15 +1,20 @@
 import Menu from './components/menu/menu';
+import Headerlinks from './components/headerlinks/headerlinks';
 import Tabs from './components/tabs/tabs';
 import Demo from './components/demo/demo';
 import SmoothScrollTo from './components/smoothscrollto/smoothScrollTo';
 
 const menu = new Menu();
+const headerlinks = new Headerlinks();
+const tabs = new Tabs('js-tabs--main');
 const tabsDemo = new Tabs('js-tabs--demo');
 const demo = new Demo();
 const smoothScrollTo = new SmoothScrollTo('js-smooth-links');
 
 document.addEventListener('DOMContentLoaded', () => {
+  headerlinks.init();
   menu.init();
+  tabs.init();
   tabsDemo.init();
   demo.init();
   smoothScrollTo.init();
