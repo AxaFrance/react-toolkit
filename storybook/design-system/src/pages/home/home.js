d2ec5e0b89b6d2236f2a3425f176dc958dec7eb5
Samuel Gomez <contact@samuelgomez.fr>
2020-10-06 01:46:31 +0200

    feat(home): update home page

--- storybook/design-system/src/pages/home/home.js
+++ storybook/design-system/src/pages/home/home.js
@@ -1,5 +1,6 @@
 const data = {
-  intro: `A "living Design System" is a web document that shows all components interface elements(UI).It informs about the typography, the colors, the grid, the iconography, the interactions ...`,
+  intro:
+    'Découvrez notre Design System spécialement conçu pour développer des applicatifs métier. Vous trouverez toutes ressources nécessaires à cet effet (guidelines UX, icones, couleur, typographie, code source, ...).',
 };
 
 module.exports = data;
