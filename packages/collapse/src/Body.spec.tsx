2c004c6c2941e561c66f834764a658778b7d9143
Arnaud Foraison <arnaud.foraison@axa.fr>
2020-10-16 10:58:23 +0200

    feat(collapse): migrate to ts (#644)

--- packages/collapse/src/Body.spec.tsx
+++ packages/collapse/src/Body.spec.tsx
@@ -1,5 +1,5 @@
+import * as React from 'react';
 import { shallow } from 'enzyme';
-import React from 'react';
 import Body from './Body';
 
 describe('Accordion.Body', () => {
---
+++ packages/collapse/src/CollapseCard.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+
+import { Constants } from '@axa-fr/react-toolkit-core';
+import CollapseCardBase from './CollapseCardBase';
+import Body from './Body';
+import Header from './Header';
+
+const defaultProps = {
+  ...Constants.defaultProps,
+  defaultCollapse: true,
+  index: 0,
+};
+
+type Props = Partial<typeof defaultProps> & {
+  children?: React.ReactNode[];
+  title?: string;
+  id?: string;
+};
+
+const CollapseCard = ({
+  title,
+  className,
+  id,
+  classModifier,
+  defaultCollapse,
+  children,
+  index,
+}: Props) => {
+  const [collapse, setCollapse] = React.useState(defaultCollapse);
+
+  return (
+    <CollapseCardBase
+      collapse={collapse}
+      onToggle={() => setCollapse(!collapse)}
+      title={title}
+      className={className}
+      id={id}
+      classModifier={classModifier}
+      index={index}>
+      {children}
+    </CollapseCardBase>
+  );
+};
+
+CollapseCard.defaultProps = defaultProps;
+
+CollapseCard.Body = Body;
+CollapseCard.Header = Header;
+
+export default CollapseCard;
