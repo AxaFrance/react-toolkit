951fdb0afe1c81ebe07c4a4745563eb7293787c3
Johnathan MEUNIER <jonathan.meunier@axa.fr>
2020-10-30 15:22:59 +0100

    feat(Number): the number component now use a input number

--- packages/Form/Input/number/src/Number.js
+++ packages/Form/Input/number/src/Number.js
@@ -8,40 +8,31 @@ import {
 } from '@axa-fr/react-toolkit-form-core';
 import { InputManager } from '@axa-fr/react-toolkit-core';
 
-import parseValueToNumber from './NumberHelper';
-
 const omitProperties = omit(['classModifier', 'className', 'isVisible']);
 
-const CustomNumber = (props) => {
-  const {
-    componentClassName,
-    id,
-    name,
-    viewValue,
-    value,
-    onChange,
-    onBlur,
-    onFocus,
-    readOnly,
-    disabled,
-    placeholder,
-    inputRef,
-    ...otherProps
-  } = props;
-  let currentViewValue = '';
-  if (value !== undefined && value !== null) {
-    currentViewValue = value.toString();
-  } else if (viewValue !== null && viewValue !== undefined) {
-    currentViewValue = props.viewValue;
-  }
+const CustomNumber = ({
+  componentClassName,
+  id,
+  name,
+  viewValue,
+  value,
+  onChange,
+  onBlur,
+  onFocus,
+  readOnly,
+  disabled,
+  placeholder,
+  inputRef,
+  ...otherProps
+}) => {
   const inputId = InputManager.getInputId(id);
   return (
     <input
       className={componentClassName}
       name={name}
       id={inputId}
-      type="text"
-      value={currentViewValue}
+      type="number"
+      value={value}
       onChange={onChange}
       onBlur={onBlur}
       onFocus={onFocus}
@@ -70,17 +61,13 @@ const defaultProps = {
 };
 
 const handlers = {
-  onChange: ({ onChange, name, id }) => (e) => {
-    const viewValue = e.target.value;
-    const value = parseValueToNumber(viewValue);
-
+  onChange: ({ onChange, name, id }) => (e) =>
     onChange({
-      value,
-      viewValue,
+      value: e.target.value,
+      viewValue: e.target.value,
       name,
       id,
-    });
-  },
+    }),
 };
 
 const EnhancedComponent = withInput(
