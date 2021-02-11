24236b32347156861c33356da9aa41c6b6da8726
Arnaud Foraison <arnaud.foraison@axa.fr>
2021-02-11 19:36:15 +0100

    Feature/migrate ts form select (#797)
    * Migration Typescript form core (#709)
    
    * feat: init form core migration in typescript
    
    * build: vscode use ts version from node_modules
    
    * feat(form): migrate ts core
    
    * fix(storybook): add empty options for storybook-readme fix
    
    Co-authored-by: Arnaud Foraison <arnaud.foraison@gmail.com>
    
    * feat(form-select): migrate to ts
    
    Co-authored-by: barry-thierno <45846776+barry-thierno@users.noreply.github.com>

--- packages/Form/Input/select/src/index.ts
+++ packages/Form/Input/select/src/index.ts
@@ -1,4 +1,3 @@
 export { default as Select } from './Select';
 export { default as SelectBase } from './SelectBase';
-export { default as SelectModes } from './SelectModes';
 export { default as SelectInput } from './SelectInput';
