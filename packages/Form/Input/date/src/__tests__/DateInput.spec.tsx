ed9b47cdbe2d50bf34b661bae9e3bfc59695afa5
Arnaud Foraison <arnaud.foraison@axa.fr>
2021-03-12 15:34:35 +0100

    feat(date): migrate to ts

--- packages/Form/Input/date/src/__tests__/DateInput.spec.tsx
+++ packages/Form/Input/date/src/__tests__/DateInput.spec.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import DateInput from './DateInput';
+import DateInput from '../DateInput';
 
 describe('<DateInput>', () => {
   it('renders DateInput correctly', async () => {
