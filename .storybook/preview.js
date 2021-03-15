8f6473a865b08721041037d9e19fe58f2a108ff6
Arnaud Foraison <arnaud.foraison@axa.fr>
2021-03-15 07:15:53 +0100

    fix(storybook): add css not scss (#894)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,7 @@ import { withKnobs } from '@storybook/addon-knobs';
 import { addReadme } from 'storybook-readme';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
-import '../packages/all/dist/style/af-toolkit-core.scss';
+import '../packages/all/dist/style/af-toolkit-core.css';
 import '../packages/core/dist/assets/fonts/icons/af-icons.css';
 import './storybook.css';
 
