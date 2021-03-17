60d7aedaa8798a84de7e5048650d46b6e4d29bd9
Arnaud Foraison <arnaud.foraison@axa.fr>
2021-03-17 18:11:54 +0100

    feat(storybook): remove header only on publish

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,7 +4,6 @@ import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import '../packages/all/dist/style/af-toolkit-core.css';
 import '../packages/core/dist/assets/fonts/icons/af-icons.css';
-import './storybook.css';
 
 export const decorators = [withKnobs, addReadme];
 
