3c804a82d67177301734c01da68a249be3a3b092
Johnathan MEUNIER <johnathan2693@gmail.com>
2021-04-02 11:26:39 +0200

    Update manager.js

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming/create';
 
 const theme = create({
   base: 'light',
-  brandTitle: '@axa-fr/react-toolkit-all',
+  brandTitle: '@axa-fr/react-toolkit-all Netlify TEST DEPLOY PR',
   brandUrl: 'https://axaguildev.github.io/design-system/',
 });
 
