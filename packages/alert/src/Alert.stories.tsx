c24a9eab87d69517a1e412d454a6387c0d43cd9c
Samuel Gomez <samuel.gomez@axa.fr>
2021-11-11 15:59:45 +0100

    docs(alert): update stories with new format

--- packages/alert/src/Alert.stories.tsx
+++ packages/alert/src/Alert.stories.tsx
@@ -1,73 +1,66 @@
-import React from 'react';
-import { text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
+import React, { ComponentProps } from 'react';
+import { Story } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Alert from './Alert';
 import readme from '../README.md';
 
-const LABELS = {
-  listClass: ['success', 'info', 'danger', 'error'],
+const MODIFIERS = ['success', 'info', 'danger', 'error'];
+
+const story = {
+  title: 'Components/Alert',
+  component: Alert,
+  parameters: {
+    readme: {
+      sidebar: readme,
+    },
+    options: {},
+  },
+  args: {},
+  argTypes: {
+    classModifier: {
+      control: { type: 'select', options: MODIFIERS },
+    },
+  },
+};
+export default story;
+
+type AlertProps = ComponentProps<typeof Alert>;
+const Template: Story<AlertProps> = (args) => <Alert {...args} />;
+
+export const AlertStory: Story<AlertProps> = Template.bind({});
+AlertStory.storyName = 'Default';
+AlertStory.args = {
+  classModifier: 'danger',
+  title: 'Attention: des informations sont manquantes',
 };
 
-const KNOBS_LABELS = {
-  classModifier: 'classModifier',
-  title: 'label',
+export const AlertInfoStory: Story<AlertProps> = Template.bind({});
+AlertInfoStory.storyName = 'Alert Info';
+AlertInfoStory.args = {
+  classModifier: 'info',
+  title: 'Info: vous pouvez également contacter un conseiller par téléphone',
 };
 
-const AlertStory = () => (
-  <Alert
-    classModifier={text(KNOBS_LABELS.classModifier, LABELS.listClass[2])}
-    icon={text('icon', '')}
-    title={text('title', 'Attention: des informations sont manquantes')}>
-    {text('children', '')}
-  </Alert>
-);
+export const AlertWithCloseStory: Story<AlertProps> = Template.bind({});
+AlertWithCloseStory.storyName = 'Alert with close';
+AlertWithCloseStory.args = {
+  classModifier: 'success',
+  title: 'Succès: votre demande a bien été enregistrée.',
+  onClose: action('onClose'),
+};
 
-const AlertInfoStory = () => (
-  <Alert
-    classModifier={text(KNOBS_LABELS.classModifier, LABELS.listClass[1])}
-    icon={text('icon', '')}
-    title={text(
-      'title',
-      'Info: vous pouvez également contacter un conseiller par téléphone'
-    )}>
-    {text('children', '')}
-  </Alert>
-);
-
-const AlertWithCloseStory = () => (
-  <Alert
-    classModifier={text('classModifier', LABELS.listClass[0])}
-    icon={text('icon', '')}
-    title={text('title', 'Succès: votre demande a bien été enregistrée.')}
-    onClose={action('onClose')}
-  />
-);
-
-const AlertWithDetailStory = () => (
-  <Alert
-    classModifier={text('classModifier', LABELS.listClass[3])}
-    icon={text('icon', 'minus-sign')}
-    title={text('title', 'Erreur dans les champs suivants :')}>
+export const AlertWithDetailStory: Story<AlertProps> = Template.bind({});
+AlertWithDetailStory.storyName = 'Alert with details';
+AlertWithDetailStory.args = {
+  classModifier: 'error',
+  title: 'Erreur dans les champs suivants :',
+  icon: 'minus-sign',
+  children: (
     <ul>
       <li>Le nom est obligatoire</li>
       <li>Le prénom est obligatoire</li>
       <li>L'email est obligatoire</li>
       <li>Le format de date est invalide</li>
     </ul>
-  </Alert>
-);
-
-const stories = storiesOf('Components/Alert', module);
-
-stories.addParameters({
-  readme: {
-    sidebar: readme,
-  },
-  options: {},
-});
-
-stories.add('Default', AlertStory);
-stories.add('Info', AlertInfoStory);
-stories.add('With close button', AlertWithCloseStory);
-stories.add('With child and custom icon', AlertWithDetailStory);
+  ),
+};
