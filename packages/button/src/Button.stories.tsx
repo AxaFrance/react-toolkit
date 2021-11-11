577238f77ede00ce7aeff7c59e00fce89fbe30a1
Samuel Gomez <samuel.gomez@axa.fr>
2021-11-11 16:00:50 +0100

    docs(button): update stories with new format

--- packages/button/src/Button.stories.tsx
+++ packages/button/src/Button.stories.tsx
@@ -1,131 +1,113 @@
-import React from 'react';
+import React, { ComponentProps } from 'react';
 import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
+import { Story } from '@storybook/react';
 import Button from './Button';
 import readme from '../README.md';
 
-const LABELS = {
-  listClass: ['success', 'info', 'danger', 'error'],
-  defaultClass: 'success',
-  title: 'Lorem ipsum',
-};
+const MODIFIERS = [
+  'success',
+  'info',
+  'danger',
+  'error',
+  'reverse',
+  'disabled',
+  'small',
+  'hasiconLeft',
+  'hasiconRight',
+];
 
-const KNOBS_LABELS = {
-  classModifier: 'classModifier',
-  title: 'label',
-};
-
-const ButtonStory = () => (
-  <Button id="id" onClick={action('onClick Action')}>
-    <span className="af-btn__text">
-      {text(KNOBS_LABELS.title, LABELS.title)}
-    </span>
-  </Button>
-);
-
-const ButtonReverseStory = () => (
-  <Button
-    id="id"
-    classModifier={text(KNOBS_LABELS.classModifier, 'reverse')}
-    onClick={action('onClick Action')}>
-    <span className="af-btn__text">
-      {text(KNOBS_LABELS.title, LABELS.title)}
-    </span>
-  </Button>
-);
-
-const ButtonSuccessStory = () => (
-  <Button
-    id="id"
-    classModifier={text(KNOBS_LABELS.classModifier, 'success')}
-    onClick={action('onClick Action')}>
-    <span className="af-btn__text">
-      {text(KNOBS_LABELS.title, LABELS.title)}
-    </span>
-  </Button>
-);
-
-const ButtonDangerStory = () => (
-  <Button
-    id="id"
-    classModifier={text(KNOBS_LABELS.classModifier, 'danger')}
-    onClick={action('onClick Action')}>
-    <span className="af-btn__text">
-      {text(KNOBS_LABELS.title, LABELS.title)}
-    </span>
-  </Button>
-);
-
-const ButtonSmallStory = () => (
-  <Button
-    id="id"
-    classModifier={text(KNOBS_LABELS.classModifier, 'small')}
-    onClick={action('onClick Action')}>
-    <span className="af-btn__text">{text(KNOBS_LABELS.title, 'OK')}</span>
-  </Button>
-);
-
-const ButtonDisabledStory = () => (
-  <Button
-    id="id"
-    classModifier={text(KNOBS_LABELS.classModifier, 'disabled')}
-    disabled
-    onClick={action('onClick Action')}>
-    <span className="af-btn__text">
-      {text(KNOBS_LABELS.title, LABELS.title)}
-    </span>
-  </Button>
-);
-
-const ButtonWithLeftIconStory = () => (
-  <Button
-    id="id"
-    classModifier={text(KNOBS_LABELS.classModifier, 'hasiconLeft')}
-    onClick={action('onClick Action')}>
-    <span className="af-btn__text">
-      {text(KNOBS_LABELS.title, LABELS.title)}
-    </span>
-    <i className={text('glyphicon', 'glyphicon glyphicon-arrowthin-left')} />
-  </Button>
-);
-
-const ButtonWithRightIconStory = () => (
-  <Button
-    id="id"
-    classModifier={text(KNOBS_LABELS.classModifier, 'hasiconRight')}
-    onClick={action('onClick Action')}>
-    <span className="af-btn__text">
-      {text(KNOBS_LABELS.title, LABELS.title)}
-    </span>
-    <i className={text('glyphicon', 'glyphicon glyphicon-arrowthin-right')} />
-  </Button>
-);
-
-const ButtonCircleStory = () => (
-  <Button
-    id="id"
-    className={text('className', 'af-btn--circle')}
-    onClick={action('onClick Action')}>
-    <i className={text('glyphicon', 'glyphicon glyphicon-floppy-disk')} />
-  </Button>
-);
-
-const stories = storiesOf('Components/Button', module);
-
-stories.addParameters({
-  readme: {
-    sidebar: readme,
+const story = {
+  title: 'Components/Button',
+  component: Button,
+  parameters: {
+    readme: {
+      sidebar: readme,
+    },
+    options: {},
   },
-  options: {},
-});
+  args: {
+    onClick: action('onClick'),
+  },
+  argTypes: {
+    classModifier: {
+      control: { type: 'select', options: MODIFIERS },
+    },
+  },
+};
+export default story;
 
-stories.add('Simple button', ButtonStory);
-stories.add('Button reverse', ButtonReverseStory);
-stories.add('Button disabled', ButtonDisabledStory);
-stories.add('Button success', ButtonSuccessStory);
-stories.add('Button danger', ButtonDangerStory);
-stories.add('Button small', ButtonSmallStory);
-stories.add('Button circle', ButtonCircleStory);
-stories.add('Button with left icon', ButtonWithLeftIconStory);
-stories.add('Button with right icon', ButtonWithRightIconStory);
+type ButtonProps = ComponentProps<typeof Button>;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const ButtonStory: Story<ButtonProps> = Template.bind({});
+ButtonStory.storyName = 'Button classic';
+ButtonStory.args = {
+  children: <span className="af-btn__text">Button classic</span>,
+};
+
+export const ButtonReverseStory: Story<ButtonProps> = Template.bind({});
+ButtonReverseStory.storyName = 'Button reverse';
+ButtonReverseStory.args = {
+  classModifier: 'reverse',
+  children: <span className="af-btn__text">Button reverse</span>,
+};
+
+export const ButtonSuccessStory: Story<ButtonProps> = Template.bind({});
+ButtonSuccessStory.storyName = 'Button success';
+ButtonSuccessStory.args = {
+  classModifier: 'success',
+  children: <span className="af-btn__text">Button success</span>,
+};
+
+export const ButtonDangerStory: Story<ButtonProps> = Template.bind({});
+ButtonDangerStory.storyName = 'Button danger';
+ButtonDangerStory.args = {
+  classModifier: 'danger',
+  children: <span className="af-btn__text">Button danger</span>,
+};
+
+export const ButtonSmallStory: Story<ButtonProps> = Template.bind({});
+ButtonSmallStory.storyName = 'Button small';
+ButtonSmallStory.args = {
+  classModifier: 'small',
+  children: <span className="af-btn__text">OK</span>,
+};
+
+export const ButtonDisabledStory: Story<ButtonProps> = Template.bind({});
+ButtonDisabledStory.storyName = 'Button disabled';
+ButtonDisabledStory.args = {
+  disabled: true,
+  classModifier: 'disabled',
+  children: <span className="af-btn__text">Button disabled</span>,
+};
+
+export const ButtonWithLeftIconStory: Story<ButtonProps> = Template.bind({});
+ButtonWithLeftIconStory.storyName = 'Button with left icon';
+ButtonWithLeftIconStory.args = {
+  classModifier: 'hasiconLeft',
+  children: (
+    <>
+      <span className="af-btn__text">Button with left icon</span>
+      <i className="glyphicon glyphicon-arrowthin-left" />
+    </>
+  ),
+};
+
+export const ButtonWithRightIconStory: Story<ButtonProps> = Template.bind({});
+ButtonWithRightIconStory.storyName = 'Button with right icon';
+ButtonWithRightIconStory.args = {
+  classModifier: 'hasiconRight',
+  children: (
+    <>
+      <span className="af-btn__text">Button with right icon</span>
+      <i className="glyphicon glyphicon-arrowthin-right" />
+    </>
+  ),
+};
+
+export const ButtonCircleStory: Story<ButtonProps> = Template.bind({});
+ButtonCircleStory.storyName = 'Button circle';
+ButtonCircleStory.args = {
+  className: 'af-btn--circle',
+  children: <i className="glyphicon glyphicon-floppy-disk" />,
+};
