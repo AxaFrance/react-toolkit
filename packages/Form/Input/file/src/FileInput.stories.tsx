6d03072693934f93fff87de0dd588dc9ef0e3820
Arnaud Foraison <arnaud.foraison@gmail.com>
2022-07-22 15:37:01 +0200

    refactor: change to browseLabel for file component label

--- packages/Form/Input/file/src/FileInput.stories.tsx
+++ packages/Form/Input/file/src/FileInput.stories.tsx
@@ -37,7 +37,7 @@ FileInputStory.args = {
   label: 'Image',
   name: 'placeImage',
   id: 'inputuniqueid',
-  chooseActionLabel: 'Browse',
+  browseLabel: 'Browse',
   accept: 'image/jpeg, image/png, application/*',
   helpMessage: 'Take a photo af a place',
   message: '',
