93b580450a7240fbf8ad72601ce50ab3cd5475b9
Martin Stievenart <stievenart.m@gmail.com>
2022-08-30 15:08:44 +0200

    feat(Help): add help button content in props

--- packages/help/src/Help.tsx
+++ packages/help/src/Help.tsx
@@ -1,10 +1,13 @@
-import React, { ComponentPropsWithoutRef } from 'react';
+import React, { ComponentPropsWithoutRef, ReactNode } from 'react';
 import Popover, {
   PopoverBase,
   PopoverPlacements,
 } from '@axa-fr/react-toolkit-popover';
+import { getComponentClassName } from '@axa-fr/react-toolkit-core';
 
-export type Props = ComponentPropsWithoutRef<typeof Popover>;
+export type HelpProps = ComponentPropsWithoutRef<typeof Popover> & {
+  helpButtonContent?: ReactNode;
+};
 
 const Help = ({
   className,
@@ -12,19 +15,28 @@ const Help = ({
   children,
   mode = 'click',
   placement = PopoverPlacements.right,
-}: Props) => (
-  <Popover
-    className={className}
-    classModifier={classModifier}
-    placement={placement}
-    mode={mode}>
-    <PopoverBase.Pop>{children}</PopoverBase.Pop>
-    <PopoverBase.Over>
-      <button className="btn af-btn--circle" type="button">
-        <span className="af-more-help">i</span>
-      </button>
-    </PopoverBase.Over>
-  </Popover>
-);
+  helpButtonContent = <span className="af-more-help">i</span>,
+}: HelpProps) => {
+  const buttonClassName = getComponentClassName(
+    'btn af-btn--circle',
+    classModifier,
+    ''
+  );
+
+  return (
+    <Popover
+      className={className}
+      classModifier={classModifier}
+      placement={placement}
+      mode={mode}>
+      <PopoverBase.Pop>{children}</PopoverBase.Pop>
+      <PopoverBase.Over>
+        <button className={buttonClassName} type="button">
+          {helpButtonContent}
+        </button>
+      </PopoverBase.Over>
+    </Popover>
+  );
+};
 
 export default Help;
