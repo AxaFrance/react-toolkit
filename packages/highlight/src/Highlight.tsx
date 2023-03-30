c9f2651259f30e043be410cd6db29ead01a9ce7c
Martin Stievenart <stievenart.m@gmail.com>
2023-03-30 16:40:58 +0200

    refactor: delete highlight component

--- packages/highlight/src/Highlight.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Highlight = () => <div />;
-
-export default Highlight;
--- packages/highlight/src/__tests__/Highlight.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Highlight from '..';
-
-describe('<Highlight />', () => {
-  it('renders without crashing', () => {
-    render(<Highlight />);
-  });
-});
