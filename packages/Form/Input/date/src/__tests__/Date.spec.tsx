f6dcf14d74c37a8514756082f3395aed3abd224e
Youssef ELAMMARI <B440UP@login.axa>
2024-03-05 14:40:29 +0100

    test(date): add test cases including a test of fix for onChange with null value when date input is cleared

---
+++ packages/Form/Input/date/src/__tests__/Date.spec.tsx
@@ -0,0 +1,86 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import CustomDate from '../Date';
+
+describe('Date component', () => {
+  it('renders with initial value', () => {
+    const initialValue = new Date('2024-03-04');
+    const { asFragment, getByDisplayValue } = render(
+      <CustomDate value={initialValue} />
+    );
+
+    getByDisplayValue('2024-03-04');
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('calls onChange handler when input value changes', () => {
+    const onChangeMock = jest.fn();
+    const initialValue = new Date('2024-03-04');
+    const { getByLabelText, asFragment } = render(
+      <>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="date">Date:</label>
+        <CustomDate
+          onChange={onChangeMock}
+          name="date"
+          id="date"
+          value={initialValue}
+        />
+      </>
+    );
+    const inputElement = getByLabelText('Date:') as HTMLInputElement;
+
+    userEvent.type(inputElement, '2024-03-05');
+
+    expect(onChangeMock).toHaveBeenCalledWith({
+      value: new Date('2024-03-05'),
+      name: 'date',
+      id: 'date',
+    });
+
+    expect(inputElement.value).toBe('2024-03-05');
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('calls onChange handler when input value changes although target value is null', () => {
+    const initialValue = new Date('2024-03-04');
+    const onChangeMock = jest.fn();
+    const { getByLabelText, asFragment } = render(
+      <>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="date">Date:</label>
+        <CustomDate
+          onChange={onChangeMock}
+          name="date"
+          id="date"
+          value={initialValue}
+        />
+      </>
+    );
+    const inputElement = getByLabelText('Date:') as HTMLInputElement;
+
+    userEvent.clear(inputElement);
+
+    expect(inputElement.value).toBe('');
+
+    expect(onChangeMock).toHaveBeenCalledWith({
+      value: null,
+      name: 'date',
+      id: 'date',
+    });
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('applies class modifier', () => {
+    const { container, asFragment } = render(
+      <CustomDate classModifier="required" />
+    );
+    container.querySelector('.af-form__input-date--required');
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
